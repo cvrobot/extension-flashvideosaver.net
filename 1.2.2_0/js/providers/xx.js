c5018b801cf07c68ef00fd7d8766f8a0e54d2bc1
lanbo gao <lanbo.gao@gmail.com>
2020-07-22 22:16:48 +0800

    Init from https://chrome.google.com/webstore/detail/free-video-d%D0%BEwnl%D0%BEader/lmieilamoollaknppoffbmdgdcolcafa?hl=en-US

---
+++ 1.2.2_0/js/providers/xx.js
@@ -0,0 +1,1 @@
+const XXProvider=class extends AbstractProvider{search(){chrome.runtime.sendMessage({action:"getTabVideos"},a=>{a&&a.length!==this.videos.length&&(this.videos=a.map(a=>({vid:a.vid,title:a.title,url:a.url,provider:"xx",quality:a.type})),this.setBadge())})}};
